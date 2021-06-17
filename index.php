c093a3fba041e2555d44224aa4949be2862da79c
Jonas Holfeld <klatschpauke@gmail.com>
2021-06-17 14:53:30 +0200

    refactored index.php

--- index.php
+++ index.php
@@ -1,30 +1,40 @@
 <?php
 
 Kirby::plugin('jonasholfeld/many-to-many-field', [
+
     'fields' => [
         'manytomany' => [
             'extends' => 'structure',
         ],
     ],
+
     'validators' => [
         'unique' => function ($value, $field) {
             $values = array_column(YAML::decode($value));
             return count($value) == count(array_unique($value, SORT_REGULAR));
         },
     ],
+
     'hooks' => [
+
         'page.update:after' => function ($newPage, $oldPage) {
+
             $relationFields = getRelationFields($newPage);
+
             // Checks if the relation field is present in the updated page
             foreach ($relationFields as $relation) {
+
                 // Checks if the relation field was edited
                 if (relationIsChanged($newPage, $oldPage, $relation)) {
+
                     // Getting Content type and page from the blueprint of the updated page
                     $relatedTemplate = $newPage->blueprint()->field($relation)['relatedTemplate'];
                     $relatedPage = page($newPage->blueprint()->field($relation)['relatedPage']);
                     $relationField = $newPage->blueprint()->field($relation)['relatationField'];
+
                     // Getting the autoid value of the updated page
                     $primaryKey = $newPage->AUTOID();
+
                     foreach ($newPage->$relation()->toStructure() as $singleRelation) {
                         // Fetching the related subpage
                         $foreign_subPage = $relatedPage->childrenAndDrafts()->findBy('autoid', $singleRelation->toArray()['foreignkey']);
@@ -36,17 +46,23 @@ Kirby::plugin('jonasholfeld/many-to-many-field', [
                         // Adding relation to foreign subpage
                         addRelation($foreign_subPage, $singleRelationAtForeign, $relationField);
                     }
+
+                    // Filtering deleted keys
                     $oldForeignKeys = $oldPage->$relation()->toStructure();
                     $newForeignKeys = $newPage->$relation()->toStructure();
                     $deletedForeignKeys = array_filter($oldForeignKeys->toArray(), function($oldForeignKey) {
                         return !in_array($oldForeignKey, $newForeignKeys->toArray());
                     });
+
                     foreach ($deletedForeignKeys as $foreignKey) {
+
                         //Finding the related subpage
                         $foreign_subPage = $relatedPage->childrenAndDrafts()->findBy('autoid', $foreignKey['foreignkey']);
+                        
                         //Chaning the relation-entry so it matches the entry at subpage
                         $singleRelationAtForeign = $foreignKey;
                         $singleRelationAtForeign['foreignkey'] = $primaryKey;
+
                         // Deleting the id field set by the toArray() Method
                         unset($singleRelationAtForeign['id']);
                         deleteRelation($foreign_subPage, $singleRelationAtForeign, $relationField);
@@ -54,22 +70,37 @@ Kirby::plugin('jonasholfeld/many-to-many-field', [
                 }
             }
         },
+
         'page.delete:before' => function ($status, $page) {
+
             $relationFields = getRelationFields($page);
+
             // Checks if the relation field is present in the updated page
             foreach ($relationFields as $relation) {
+
+                // Getting autoids of related pages
                 $foreignKeys = $page->$relation()->toStructure()->toArray();
+
+                // Getting the autoid value of the deleted page
                 $primaryKey = $page->AUTOID();
+
+                // Getting related page and relation field from the blueprint of the deleted page
                 $relatedPage = page($page->blueprint()->field($relation)['relatedPage']);
                 $relationField = $page->blueprint()->field($relation)['relatationField'];
+
                 foreach ($foreignKeys as $foreignKey) {
-                    //Finding the related subpage
+
+                    // Finding the related subpage
                     $foreign_subPage = $relatedPage->childrenAndDrafts()->findBy('autoid', $foreignKey['foreignkey']);
-                    //Changing the relation-entry so it matches the entry at subpage
+
+                    // Changing the relation-entry so it matches the entry at subpage
                     $singleRelationAtForeign = $foreignKey;
                     $singleRelationAtForeign['foreignkey'] = $primaryKey;
+
                     // Deleting the id field set by the toArray() Method
                     unset($singleRelationAtForeign['id']);
+
+                    // Deleting the relation entry from the related page
                     deleteRelation($foreign_subPage, $singleRelationAtForeign, $relationField);
                 }
             }
@@ -80,6 +111,7 @@ Kirby::plugin('jonasholfeld/many-to-many-field', [
 function getRelationFields($page)
 {
     $relationFields = [];
+
     foreach ($page->blueprint()->fields() as $field) {
         if ($field['type'] == 'manytomany') {
             array_push($relationFields, $field['name']);
@@ -91,9 +123,14 @@ function getRelationFields($page)
 
 function deleteRelation($page, $value, $relationField)
 {
+
     // Getting relations field from page to delete from
     $fieldData = $page->$relationField()->toStructure()->toArray();
+
+    // Creating empty field
     $newFieldData = [];
+
+    // Pushing all entries that dont match the deleted relation 
     foreach ($fieldData as $relation) {
         $singleRelation = $relation;
         unset($singleRelation['id']);
@@ -101,6 +138,7 @@ function deleteRelation($page, $value, $relationField)
             array_push($newFieldData, $singleRelation);
         }
     }
+
     // Encoding
     try {
         // Updating page
@@ -112,6 +150,7 @@ function deleteRelation($page, $value, $relationField)
 
 function relationIsChanged($newPage, $oldPage, $relation)
 {
+    //Constructing safer strings for comparison
     $oldRelations = str_replace(["\n", "\r"], '', $oldPage->$relation()->toString());
     $newRelations = str_replace(["\n", "\r"], '', $newPage->$relation()->toString());
 
@@ -122,14 +161,19 @@ function addRelation($page, $value, $relationField)
 {
     // Getting relations field from page to add to
     $fieldData = YAML::decode(page($page)->$relationField()->value());
+
     // Getting Length of relations field before insert
     $fieldLengthBefore = count($fieldData);
+
     // Writing to relations field
     array_push($fieldData, $value);
+
     // Making array unique to filter out duplicates
     $fieldData = array_unique($fieldData, SORT_REGULAR);
+
     // Getting Length of relations field after insert
     $fieldLengthAfter = count($fieldData);
+    
     // If fieldLengthAfter is same as fieldLengthBefore, nothing was added so we skip updating to avoid cascading
     if ($fieldLengthBefore !== $fieldLengthAfter) {
         try {
